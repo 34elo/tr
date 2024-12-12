61ec02bffb945255e1aed38d1d831eda9ec84100
elo <evsenik23@mail.ru>
2024-12-13 01:10:06 +0300

    2nd

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-console.log("hello world")
+console.log("hello world");
+
+function f(number) {
+    for (let i = 0; i < 5; ++i) {
+        console.log(i)
+    }
+    console.log('hello, world');
+    return number;
+}
+console.log(f(1))
